1c142cd52f1f9c1dab7e6002c5df789ee8883655
chlar <choimk0512@gmail.com>
2022-06-22 19:20:40 +0900

    seventh commit

--- list_test.py
+++
@@ -1,5 +0,0 @@
-a = []
-for i in range(5):
- for j in range(3):
-  a[i][j].append(j)
-print(a)
--- refraction of lens.py
+++ refraction of lens.py
@@ -30,7 +30,7 @@ def T2(a):
 def find_x(a, t):
   if(t<f(a)):
    return a
-  elif(f(a)<=t<f2(a)):
+  elif(f(a)<=t<=f2(a)):
    return 1/T(a)*(t-f(a))+a
   else:
     return 1/T2(a)*(t-f2(contact))+contact
@@ -39,15 +39,15 @@ x_lens_d, y_lens_d = list(np.arange(-5, 5.1, 0.1)), list(f(np.arange(-5, 5.1, 0.
 x_lens_u, y_lens_u = list(np.arange(-5, 5.1, 0.1)), list(f2(np.arange(-5, 5.1, 0.1)))
 ray_x, ray_y = [], []
 
-plt.figure(figsize=(10, 10))
+plt.figure(figsize=(8, 8))
 plt.xlim([-5.5, 5.5])
 plt.ylim([-2, 25])
 plt.plot(x_lens_d, y_lens_d, color='skyblue', linewidth='3')
 plt.plot(x_lens_u, y_lens_u, color='skyblue', linewidth='3')
 
 for i in np.arange(-2, 2.5, 0.5):
-  alpha = -5*T(i)+np.sqrt(25*T(i)**2+10*T(i)*i-10*f2(i)+50)
-  beta = -5*T(i)-np.sqrt(25*T(i)**2+10*T(i)*i-10*f2(i)+50)
+  alpha = -5*T(i)+np.sqrt(25*T(i)**2+10*T(i)*i-10*f(i)+50)
+  beta = -5*T(i)-np.sqrt(25*T(i)**2+10*T(i)*i-10*f(i)+50)
   contact = alpha if f2(alpha)>=f2(beta) else beta
   for j in np.arange(-2, 30, 0.001):
     ray_x.append(find_x(i, j))
