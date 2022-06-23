c309cbe4cae28e6f35ef3609958d86ffccd13a6c
chlar <choimk0512@gmail.com>
2022-06-24 00:51:08 +0900

    thirteenth commit

--- convex lens.py
+++ convex lens.py
@@ -1,58 +1,52 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+global contact
+
 def f(x):
  return 1/10*x**2
-
-def g(x):
-  return -1/10*x**2+5
-
 def f_d(x):
  return 1/5*x
 
+def g(x):
+ return -1/10*x**2+5
 def g_d(x):
-  return -1/5*x
+ return -1/5*x
 
-def T1(x):
-  if(x<0):
-    return np.tan(np.pi/2-np.arctan(-f_d(x))+np.arcsin(2/3*np.sin(np.arctan(-f_d(x)))))
-  else:
-    return np.tan(np.pi/2+np.arctan(f_d(x))-np.arcsin(2/3*np.sin(np.arctan(f_d(x)))))
+def M1(x):
+ return np.tan(np.pi/2-np.arctan(-f_d(x))+np.arcsin(2/3*np.sin(np.arctan(-f_d(x)))))
+def M2(x):
+ return np.tan(np.arctan(-1/g_d(contact))+np.arcsin(3/2*np.sin(np.arctan(-1/g_d(contact))-np.arctan(M1(x)))))
 
-def T2(x):
-  if(contact<0):
-    return np.tan(np.arctan(-1/g_d(contact))-np.arcsin(3/2*np.sin(np.arctan(-1/g_d(contact))-np.arctan(T1(x)))))
-  else:
-    return np.tan(np.arctan(-1/g_d(contact))+np.arcsin(3/2*np.sin(-np.arctan(-1/g_d(contact))+np.arctan(T1(x)))))
+def find_x(x, y):
+ if(y<=f(x) or x<-5 or x>5):
+  return x
+ elif(f(x)<y<=g(contact)):
+  return 1/M1(x)*(y-f(x))+x
+ else:
+  return 1/M2(x)*(y-g(contact))+contact
 
-def find_x(a, t):
-  if(t<=f(a)):
-   return a
-  elif(f(a)<t<=f2(a)):
-   return 1/T(a)*(t-f(a))+a
-  else:
-    return 1/T2(a)*(t-f2(contact))+contact
-
-x_lens_d, y_lens_d = list(np.arange(-5, 5.1, 0.1)), list(f(np.arange(-5, 5.1, 0.1)))
-x_lens_u, y_lens_u = list(np.arange(-5, 5.1, 0.1)), list(f2(np.arange(-5, 5.1, 0.1)))
+lens_d = [list(np.linspace(-5, 5, 100)), list(f(np.linspace(-5, 5, 100)))]
+lens_u = [list(np.linspace(-5, 5, 100)), list(g(np.linspace(-5, 5, 100)))]
 ray_x, ray_y = [], []
 
-plt.figure(figsize=(7, 5))
-plt.xlim([-5.5, 5.5])
-plt.ylim([-2, 20])
-plt.plot(x_lens_d, y_lens_d, color='skyblue', linewidth='8', zorder=1)
-plt.plot(x_lens_u, y_lens_u, color='skyblue', linewidth='8', zorder=1)
+plt.figure(figsize=(6, 6))
+plt.axis([-5.5, 5.5, -3, 20])
+plt.xticks(range(0), range(0))
+plt.yticks(range(0), range(0))
+plt.title("convex lens")
+plt.plot(lens_d[0], lens_d[1], color='skyblue', linewidth='3', zorder=1)
+plt.plot(lens_u[0], lens_u[1], color='skyblue', linewidth='3', zorder=1)
 
-for i in np.linspace(-3, 3, 25):
-  alpha = -5*T(i)+np.sqrt(25*T(i)**2+10*T(i)*i-10*f(i)+50)
-  beta = -5*T(i)-np.sqrt(25*T(i)**2+10*T(i)*i-10*f(i)+50)
-  contact = alpha if f2(alpha)>=f2(beta) else beta
-  for j in np.arange(-2, 20, 0.01):
-    ray_x.append(find_x(i, j))
-    ray_y.append(j)
-  plt.plot(ray_x, ray_y, color='red', linewidth='1', zorder=0)
-  ray_x.clear()
-  ray_y.clear()
-  print("%lf"%contact)
+for x in np.linspace(-3, 3, 50):
+ alpha = -5*M1(x)+np.sqrt(25*M1(x)**2+10*M1(x)*x-10*f(x)+50)
+ beta = -5*M1(x)-np.sqrt(25*M1(x)**2+10*M1(x)*x-10*f(x)+50)
+ contact = alpha if g(alpha)>g(beta) else beta
+ for y in np.arange(-3, 20, 0.1):
+  ray_x.append(find_x(x, y))
+  ray_y.append(y)
+ plt.plot(ray_x, ray_y, color='red', linewidth='0.5', zorder=0)
+ ray_x.clear()
+ ray_y.clear()
 
 plt.show()
