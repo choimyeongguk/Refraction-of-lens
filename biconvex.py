84e1c4f2b08ce7e7888bf63f3c5d992413d7650e
chlar <choimk0512@gmail.com>
2022-06-27 12:39:13 +0900

    seventeenth commit

--- biconvex.py
+++ biconvex.py
@@ -11,12 +11,12 @@ def f_d(x):
 def g(x):
  return -1/10*x**2+5
 def g_d(x):
- return -1/5*x
+ return -1/5*x  
 
 def M1(x):
  return np.tan(np.pi/2-np.arctan(-f_d(x))+np.arcsin(2/3*np.sin(np.arctan(-f_d(x)))))
 def M2(x):
- return np.tan(np.arctan(-1/g_d(contact))+np.arcsin(3/2*np.sin(np.arctan(-1/g_d(contact))-np.arctan(M1(x)))))
+ return -np.tan(np.arcsin(3/2*np.sin(np.arctan(1/g_d(contact))+np.arctan(M1(x))))+np.arctan(1/g_d(contact)))
 
 def find_x(x, y):
  if(y<=f(x) or x<-5 or x>5):
@@ -38,7 +38,7 @@ plt.title("convex lens")
 plt.plot(lens_d[0], lens_d[1], color='skyblue', linewidth='3', zorder=0)
 plt.plot(lens_u[0], lens_u[1], color='skyblue', linewidth='3', zorder=0)
 
-for x in np.linspace(-5.5, 5.5, 50):
+for x in np.linspace(-5.5, 5.5, 100):
  alpha = -5*M1(x)+np.sqrt(25*M1(x)**2+10*M1(x)*x-10*f(x)+50)
  beta = -5*M1(x)-np.sqrt(25*M1(x)**2+10*M1(x)*x-10*f(x)+50)
  contact = alpha if g(alpha)>g(beta) else beta
